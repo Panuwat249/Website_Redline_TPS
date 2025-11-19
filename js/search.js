c4b27c5993a7c25f81924aa5883af74bc691d104
KhaiDao <129141028+Panuwat249@users.noreply.github.com>
2025-11-20 00:33:56 +0700

    Delete js directory

--- js/search.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('searchInput').addEventListener('keyup', function() {
-    let filter = this.value.toLowerCase();
-    let rows = document.querySelectorAll('#trainTable tbody tr');
-    rows.forEach(row => {
-        let station = row.cells[0].textContent.toLowerCase();
-        row.style.display = station.includes(filter) ? '' : 'none';
-    });
-});
